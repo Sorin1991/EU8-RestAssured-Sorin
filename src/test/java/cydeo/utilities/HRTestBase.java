3adfc983d0ae7e18240bad7cfae2d72096248a28
Sorin1991 <basco91@gmail.com>
2022-07-20 22:18:38 +0100

    Day3 SpartanTestWithPath

---
+++ src/test/java/cydeo/utilities/HRTestBase.java
@@ -0,0 +1,16 @@
+package cydeo.utilities;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public abstract class HRTestBase {
+
+    @BeforeAll
+    public static void init() {
+        //save baseurl inside this variable so that we don't need to type each http method
+        baseURI = "http://52.91.79.87:1000/ords/hr";
+    }
+
+
+}
---
+++ src/test/java/cydeo/utilities/SpartanTestBase.java
@@ -0,0 +1,14 @@
+package cydeo.utilities;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public abstract class SpartanTestBase {
+
+    @BeforeAll
+    public static void init() {
+        //save baseurl inside this variable so that we don't need to type each http method
+        baseURI = "http://52.91.79.87:8000";
+    }
+}
