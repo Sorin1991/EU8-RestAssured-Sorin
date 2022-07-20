4dc95da8c60f288323471f179905fd76458e3ed7
Sorin1991 <basco91@gmail.com>
2022-07-20 19:09:26 +0100

    Day3 SpartanTestWithPath

--- src/test/java/cydeo/day3/SpartanTestsWithPath.java
+++ src/test/java/cydeo/day3/SpartanTestsWithPath.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -65,8 +67,38 @@ public class SpartanTestsWithPath {
         assertEquals("Lorenza",name);
         assertEquals("Female",gender);
         assertEquals(331280936l,phone);
+    }
 
+    @DisplayName("GET all spartan and navigate with Path()")
+    @Test
+    public void test2(){
+
+
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/api/spartans");
+
+        //response.prettyPrint();
+
+        int firstId = response.path("id[0]");
+        System.out.println("firstId = " + firstId);
+
+        String name = response.path("name[0]");
+        System.out.println("name = " + name);
+
+        String lastFirstName = response.path("name[-1]");
+        System.out.println("lastFirstName = " + lastFirstName);
+
+        //save names inside the list of string
+        List<String> names = response.path("name");
+        System.out.println(names);
+
+        //print each ame one by one
+        for(String n :names){
+            System.out.println(n);
+        }
 
     }
+
+
 
 }
