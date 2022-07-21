e1ecb5a6c5a9665a416bc69af0920b0791ce4ddf
Sorin1991 <basco91@gmail.com>
2022-07-22 00:29:57 +0100

    Day4 CB Training Api With JsonPath

--- src/test/java/cydeo/day4/CBTrainingApiWithJsonPath.java
+++ src/test/java/cydeo/day4/CBTrainingApiWithJsonPath.java
@@ -1,10 +1,16 @@
 package cydeo.day4;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CBTrainingApiWithJsonPath {
 
@@ -18,12 +24,11 @@ public class CBTrainingApiWithJsonPath {
     @Test
     public void test1(){
         //send a get request to student id 23401 as a path parameter
-        //verify status code=200 /content type=application/jso;charset=UTF -8 /Content-Encoding = gzip
+        //verify status code=200 /content type=application/json;charset=UTF -8 /Content-Encoding = gzip
         //verify Date header exists
         //assert that
 
         /*
-
             firstName Vera
             batch 14
             section 12
@@ -34,6 +39,35 @@ public class CBTrainingApiWithJsonPath {
 
             using JsonPath
          */
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id",32701)
+                .when().get("student/{id}");
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json;charset=UTF-8",response.contentType());
+        assertEquals("gzip",response.getHeader("Content-Encoding"));
+        assertTrue(response.getHeader("Date").contains("GMT"));
+
+        JsonPath jsonPath = response.jsonPath();
+
+        System.out.println("First Name = " + jsonPath.getString("students[0].firstName"));
+        System.out.println("Batch = " + jsonPath.getString("students[0].batch"));
+        System.out.println("Section = " + jsonPath.getString("students[0].section"));
+        System.out.println("emailAddress = " + jsonPath.getString("students[0].contact.emailAddress"));
+        System.out.println("Company Name = " + jsonPath.getString("students[0].company.companyName"));
+        System.out.println("State = " + jsonPath.getString("students[0].company.address.state"));
+        System.out.println("Zip Code = " + jsonPath.getString("students[0].company.address.zipCode"));
+
+        assertEquals("Vera", jsonPath.getString("students[0].firstName"));
+        assertEquals("14", jsonPath.getString("students[0].batch"));
+        assertEquals("12", jsonPath.getString("students[0].section"));
+        assertEquals("aaa@gmail.com", jsonPath.getString("students[0].contact.emailAddress"));
+        assertEquals("Cybertek", jsonPath.getString("students[0].company.companyName"));
+        assertEquals("IL", jsonPath.getString("students[0].company.address.state"));
+        assertEquals("60606", jsonPath.getString("students[0].company.address.zipCode"));
+
+
+
     }
 
 }
