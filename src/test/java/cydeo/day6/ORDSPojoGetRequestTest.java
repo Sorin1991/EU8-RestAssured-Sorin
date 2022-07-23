51d874cf72305044ba803fe3418a24c0e6dffe71
Sorin1991 <basco91@gmail.com>
2022-07-23 13:54:09 +0100

    RegionTest

---
+++ src/test/java/cydeo/day6/ORDSPojoGetRequestTest.java
@@ -0,0 +1,44 @@
+package cydeo.day6;
+
+import cydeo.pojo.Link;
+import cydeo.pojo.Region;
+import cydeo.utilities.HRTestBase;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ORDSPojoGetRequestTest extends HRTestBase {
+
+    //create pojo classes for this response
+    //then test it with sending get request ot regions endpoint and
+    //only pointing first region and converting your pojo
+
+    @Test
+    public void regionsTest(){
+
+        JsonPath jsonPath = get("/regions").then().statusCode(200).extract().jsonPath();
+
+        Region region1 = jsonPath.getObject("items[0]", Region.class);
+
+        System.out.println(region1);
+
+        System.out.println("region1.getRegion_id() = " + region1.getRegion_id());
+        System.out.println("region1.getRegion_name() = " + region1.getRegion_name());
+        System.out.println("region1.getLinks().get(0).getHref() = " + region1.getLinks().get(0).getHref());
+
+
+
+
+       /*** You can get this part too
+        Link link1 = region1.getLinks().get(0);
+        System.out.println("link1.getHref() = " + link1.getHref());
+        */
+
+    }
+
+
+
+}
--- src/test/java/cydeo/utilities/HRTestBase.java
+++ src/test/java/cydeo/utilities/HRTestBase.java
@@ -1,5 +1,6 @@
 package cydeo.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -10,6 +11,17 @@ public abstract class HRTestBase {
     public static void init() {
         //save baseurl inside this variable so that we don't need to type each http method
         baseURI = "http://52.91.79.87:1000/ords/hr";
+
+        String dbUrl = "jdbc:oracle:thin:@52.91.79.87:1521:xe";
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+        //DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+    }
+
+    @AfterAll
+    public static void teardown(){
+
+        // DBUtils.destroy();
     }
 
 
